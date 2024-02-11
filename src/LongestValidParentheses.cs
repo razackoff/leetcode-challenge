fe4b969c05af2aaa3a47e12189630b01fabaf855
firuz-macbook <razackoff@gmail.com>
2024-02-11 16:30:47 +0200

    Add solution of  Longest Valid Parentheses

---
+++ src/LongestValidParentheses.cs
@@ -0,0 +1,62 @@
+public class Solution {
+    public int LongestValidParentheses(string s) {
+        List<int> list = new List<int>();
+        int k = 1, ans = 0;
+        for(int i = 0; i < s.Length; i++){
+            if(s[i] == '('){
+                list.Add(-1);
+            }
+            else if(s[i] == ')'){
+                list.Add(1);
+            }
+        }
+        while(k > 0){
+            k--;
+            for(int i = 0; i < list.Count; i++){
+                /*ans *= 10;
+                if(list[i] == -1){
+                    ans += 3;
+                }
+                else{
+                    ans += list[i];
+                }*/
+                if(i > 0 && list[i - 1] >= 2 && list[i] >= 2){
+                    list[i - 1] = list[i - 1] + list[i];
+                    list.RemoveAt(i);
+                    i--;
+                    k++;
+                }
+                if(list[i] == 1 && i - 2 >= 0 && list[i - 2] == -1 && list[i - 1] % 2 == 0){
+                    list[i - 2] = 2;
+                    list.RemoveAt(i);
+                    i--;
+                    k++;
+                }
+                if(i - 1 >= 0 && list[i] == 1 && list[i - 1] == -1){
+                    list[i - 1] = 2;
+                    list.RemoveAt(i);
+                    k++;
+                    i--;
+                }
+            }
+            /*if(ans >= 1000000){
+                break;
+            }*/
+        }
+        //return 500;
+        ans = 0;
+        for(int i = 0; i < list.Count; i++){
+            /*ans *= 10;
+            if(list[i] == -1){
+                ans += 3;
+            }
+            else{
+                ans += list[i];
+            }*/
+            if(list[i] > ans && list[i] != 1){
+                ans = list[i];
+            }
+        }
+        return ans;
+    }
+}
