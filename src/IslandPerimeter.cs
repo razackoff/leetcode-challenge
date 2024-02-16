737dd4c385fa4ae1f619d8a9544c90cdf6f4b502
firuz-macbook <razackoff@gmail.com>
2024-02-16 20:45:42 +0200

    Add solution of Island Perimeter

---
+++ src/IslandPerimeter.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int IslandPerimeter(int[][] grid) {
+            int perimeter = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        perimeter += 4;
+
+                        if (j - 1 >= 0 && grid[i][j - 1] == 1) perimeter -= 1;
+                        if (j + 1 < grid[i].Length && grid[i][j + 1] == 1) perimeter -= 1;
+                        if (i - 1 >= 0 && grid[i - 1][j] == 1) perimeter -= 1;
+                        if (i + 1 < grid.Length && grid[i + 1][j] == 1) perimeter -= 1;
+                    }
+
+                }
+            }
+            return perimeter;
+    }
+}
